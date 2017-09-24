f25599d86b55244a3e9e03679bd85784b3413b73
Yanan Dong <yndong@thoughtworks.com@twcns-MacBook-Pro-3.local>
2017-09-24 10:21:41 +0800

    reorganized files' dir and implement container of addTodo

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../css/App.css';
+import AddTodo from "./addTodo";
 
 class App extends Component {
   render() {
@@ -10,9 +11,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Todo-list-container">
+          <AddTodo/>
+
+        </div>
       </div>
     );
   }
