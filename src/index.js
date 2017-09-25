5ad02d315e47c4781cfc4d68f8a68c15de71e437
Yanan Dong <yndong@thoughtworks.com@twcns-MacBook-Pro-3.local>
2017-09-25 20:35:42 +0800

    connect to redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 import './css/index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import Provider from "react-redux/es/components/Provider";
+import reducer from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+document.getElementById('root')
+)
+
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,3 @@
+const todoApp = () => {}
+
+export default todoApp
