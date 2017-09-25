c341cdab473159578f3535ffb8da58e55854f2d0
Yanan Dong <yndong@thoughtworks.com@twcns-MacBook-Pro-3.local>
2017-09-25 21:29:24 +0800

    implement addTodo

--- src/containers/addTodo.js
+++ src/containers/addTodo.js
@@ -1,12 +1,26 @@
 import * as React from "react";
+import connect from "react-redux/es/connect/connect";
+import {addTodo} from "../actions/index";
 
-const AddTodo = () => {
+let AddTodo = ({dispatch}) => {
+  let input;
   return (
     <div>
-      <input placeholder="Please input your todo here."/>
-      <button>ADD TODO</button>
+      <input placeholder="Please input your todo here." ref={node=>{input=node}}/>
+      <button
+        onClick={
+          ()=>{
+            !input.value.trim() || dispatch(addTodo(input.value));
+            input.value='';
+          }
+        }
+      >
+        ADD TODO
+      </button>
     </div>
   );
 }
+
+AddTodo=connect()(AddTodo);
 
 export default AddTodo;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,13 @@
-const todoApp = () => {}
+const todoApp = (state=[], action) => {
+  if(action.type=="ADD_TODO"){
+    return [
+      ...state,
+      {
+        id: action.id,
+        text: action.text
+      }
+    ]
+  }
+}
 
 export default todoApp
