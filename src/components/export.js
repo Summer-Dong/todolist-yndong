e389e8faed5a99ade6bb76067af8272d7f51e61e
Yanan Dong <yndong@thoughtworks.com@twcns-MacBook-Pro-3.local>
2017-10-02 12:59:46 +0800

    add examples of 'export default' and 'import *'

--- src/components/export.js
+++ src/components/export.js
@@ -9,7 +9,18 @@ const C='c';
 const D='d';
 export {C,D};
 
-export const sumXAndY=(x, y)=>x+y;
+export default function(x, y){ return x+y};
 
 const minus=(m, n)=>m-n;
 export {minus as minusMAndN}
+
+// //less well
+// const A='a';
+// const B='b';
+//
+// //well
+// const C='c';
+// const D='d';
+//
+// const sumXAndY=(x, y)=>x+y;
+// const minus=(m, n)=>m-n;
