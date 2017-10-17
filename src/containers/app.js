fd670187a54b3821e93cc6c0714ccfb5618f48f9
Yanan Dong <yndong@thoughtworks.com@twcns-MacBook-Pro-3.local>
2017-10-17 18:44:04 +0800

    add footer

--- src/containers/app.js
+++ src/containers/app.js
@@ -5,9 +5,27 @@ import AddTodo from './addTodo';
 import Lists from './lists';
 import Import from '../components/import';
 
-var appStyle = {
+const appStyle = {
   marginLeft: 50,
 };
+
+const footerStyle = {
+  height: 30,
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  right: 0,
+  display: 'flex',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  backgroundColor: '#efefef',
+  textAlign: 'right',
+};
+
+const footerTextStyle = {
+  paddingRight: 50,
+  color: 'black',
+}
 
 const App = () => (
   <div style={appStyle}>
@@ -24,6 +42,11 @@ const App = () => (
       <Lists />
       <Import />
     </div>
+
+    <footer style={footerStyle}>
+      <Icon name="at" />
+      <a style={footerTextStyle} href="mailto:yndong@thoughtworks.com">yndong</a>
+    </footer>
 
   </div>
 );
