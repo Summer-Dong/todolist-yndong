4b31960d9c9cec85dd6b4eda104d743819f7c73c
Yanan Dong <yndong@thoughtworks.com@twcns-MacBook-Pro-3.local>
2017-10-20 17:00:01 +0800

    update import for semantic-ui

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import Provider from 'react-redux/es/components/Provider';
+import 'semantic-ui/dist/semantic.min.css';
 import App from './containers/app';
 import reducer from './reducers';
 
