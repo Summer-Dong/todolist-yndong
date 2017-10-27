5101a1900af883993ab10d3dc21a46ae65c71021
Yanan Dong <yndong@thoughtworks.com@twcns-MacBook-Pro-3.local>
2017-10-27 09:10:06 +0800

    fix error for can't find class when DB class does't exist and page should being initialized

--- src/apis/todos.js
+++ src/apis/todos.js
@@ -3,17 +3,16 @@ import AV from 'leancloud-storage';
 const APP_ID = 'yL98ASNXWEJ0TFMYvxr2uBdO-gzGzoHsz';
 const APP_KEY = '0dIzh3OPc8NHdVy6bhNHI4nN';
 
-const TodoDB = AV.Object.extend('TodoDB');
-const todoDB = new TodoDB();
-
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY,
 });
 
-const query = new AV.Query('TodoDB');
+const TodoDB = AV.Object.extend('TodoDB');
+const todoDB = new TodoDB();
 
 export const getStateFromLeanCloud = () => {
+  const query = new AV.Query('TodoDB');
   query.include('id');
   query.include('text');
   query.include('isCompleted');
@@ -22,7 +21,8 @@ export const getStateFromLeanCloud = () => {
     .then(todos => todos.map(todo => (
       { ...todo.attributes })))
     .catch((error) => {
-      console.log(JSON.stringify(error));
+      console.log(error);
+      return [];
     });
 };
 
